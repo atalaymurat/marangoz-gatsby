4ac2ae45aa0ccb84c9de9ff183a09dd1709f7631
murat atalay <atalaymurat@msn.com>
2020-11-29 17:56:32 +0300

    changes

--- src/pages/hakkimizda.js
+++ src/pages/hakkimizda.js
@@ -6,34 +6,32 @@ import Layout from "../components/layout"
 import SEO from "../components/seo"
 import "./hakkimizda.css"
 
-const Hakkimizda = ( props ) => (
-		<BackgroundImage
-			fluid={props.data.indexImage.childImageSharp.fluid}
-			
-	>
-  <Layout>
-    <SEO title="Marangozi.com hakkinda" />
-    <Link to="/">geri</Link>
-	<div className="black-overlay">
-
-    <h1>Hakkimizda</h1>
-		<h2>Tarihce</h2>
-		<h2>Neden Ahsap</h2>
-	</div>
-  </Layout>
-		</BackgroundImage>
+const Hakkimizda = props => (
+	<BackgroundImage fluid={props.data.indexImage.childImageSharp.fluid}>
+		<Layout>
+			<SEO title="Marangozi.com hakkinda" />
+			<Link to="/">geri</Link>
+			<div className="black-overlay">
+				<h1>Hakkimizda</h1>
+				<p>hakkimizda paylasilacak bilgiler ile ilgili yazilar bu alana yazilacaktir</p>
+				<h2>Tarihce</h2>
+				<h2>Neden Ahsap</h2>
+				<h2>Basinda marangozi</h2>
+			</div>
+		</Layout>
+	</BackgroundImage>
 )
 
 export default Hakkimizda
 
 export const pageQuerry = graphql`
 	query {
-		indexImage: file(relativePath: { eq: "panjur02.jpg"}) {
-		childImageSharp {
-			fluid(maxWidth: 960){
-				...GatsbyImageSharpFluid
+		indexImage: file(relativePath: { eq: "panjur02.jpg" }) {
+			childImageSharp {
+				fluid(maxWidth: 960) {
+					...GatsbyImageSharpFluid
+				}
 			}
-		}
 		}
 	}
 `
